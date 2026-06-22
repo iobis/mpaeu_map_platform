a715c948e966e5a706e0b0f32bb79f1ab68a2e5a
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2026-06-22 08:35:29 +0200

    Update atlas

--- components/tabactions.R
+++ components/tabactions.R
@@ -29,6 +29,73 @@ observeEvent(input$jsValue, {
 })
 
 # Observe first input change
+atlas_modal_shown <- reactiveVal(FALSE)
+observeEvent(active_tab$current, {
+  if (active_tab$current == "atlas" && !atlas_modal_shown()) {
+    atlas_modal_shown(TRUE)
+    atlas_message <- htmltools::tagList(
+      htmltools::tags$style(htmltools::HTML("
+        .front-card {
+          border: solid;
+          margin-top: 15px;
+          margin-bottom: 15px;
+          margin-right: 5px;
+          margin-left: 5px;
+          padding: 3px;
+          border-radius: 10px;
+          border-color: #bcbcbc;
+          cursor: pointer;
+          transition: background-color 0.2s ease;
+        }
+        .front-card:hover { background-color: #f0f0f0; }
+        .card-icon {
+          display: flex;
+          flex-direction: row;
+          height: 100%;
+          align-items: center;
+          justify-content: center;
+          font-size: 2.1em;
+        }
+      ")),
+      htmltools::div(class = "container",
+        htmltools::div(class = "row",
+          htmltools::div(class = "g-col-12",
+            htmltools::p(htmltools::HTML(
+              "Visit the new MPA Europe atlas, with an improved experience and better layer controls."
+            ))
+            # ,
+            # bslib::card(
+            #   bslib::layout_column_wrap(
+            #     style = htmltools::css(grid_template_columns = "1fr 6fr"),
+            #     htmltools::div(
+            #       htmltools::tags$i(class = "fa-solid fa-map fa-2x", style = "color: #184e77;"),
+            #       class = "card-icon"
+            #     ),
+            #     htmltools::div(
+            #       htmltools::h4(htmltools::HTML(
+            #         '<span style="color: #184e77;">MPA Europe Atlas</span> <i class="fa-solid fa-arrow-up-right-from-square" style="font-size: 0.85em; color: #b2b2b2;"></i>'
+            #       ), style = "margin-top: 0px !important;"),
+            #       htmltools::p("A dedicated app with more layers, advanced filtering, and a better overall experience."),
+            #       style = "text-align: left;"
+            #     )
+            #   ),
+            #   class = "front-card"
+            # )
+          )
+        )
+      )
+    )
+    shinyalert::shinyalert(
+      "A new atlas is available",
+      atlas_message,
+      type = "info", html = TRUE, size = "m", closeOnClickOutside = TRUE,
+      confirmButtonText = "Explore the new atlas", confirmButtonCol = "#184e77",
+      showCancelButton = TRUE, cancelButtonText = "Stay here",
+      callbackJS = "function(x) { if (x) { window.open('https://iobis.github.io/mpaeu_atlas/', '_blank'); } }"
+    )
+  }
+}, ignoreInit = TRUE)
+
 # Special case of atlas
 atlas_active <- reactiveVal(FALSE)
 bindEvent(observe({
